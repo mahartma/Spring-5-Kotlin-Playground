ef3e1555b672c27e14bd6c8994475e2b983beb35
mahartma <max.hartmann@codecentric.de>
2019-01-04 14:21:52 +0100

    init commit, spring 5, kotlin

---
+++ src/main/java/de/codecentric/workshop/spring5/WebApp.kt
@@ -0,0 +1,91 @@
+package de.codecentric.workshop.spring5
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.amqp.core.Queue
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.integration.amqp.dsl.Amqp
+import org.springframework.integration.channel.PublishSubscribeChannel
+import org.springframework.integration.dsl.IntegrationFlow
+import org.springframework.integration.dsl.IntegrationFlows
+import org.springframework.messaging.Message
+import org.springframework.messaging.MessageHandler
+import org.springframework.messaging.SubscribableChannel
+import org.springframework.web.reactive.HandlerMapping
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping
+import org.springframework.web.reactive.socket.WebSocketHandler
+import org.springframework.web.reactive.socket.WebSocketMessage
+import org.springframework.web.reactive.socket.WebSocketSession
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter
+import reactor.core.publisher.Flux
+import reactor.core.publisher.FluxSink
+import java.util.function.Consumer
+
+@SpringBootApplication
+class WebApp {
+
+    @Bean
+    fun inboundChannel() = PublishSubscribeChannel()
+
+    @Bean
+    fun rabbitFlow(connectionFactory: ConnectionFactory, inboundChannel: SubscribableChannel): IntegrationFlow =
+            IntegrationFlows
+            .from(Amqp.inboundAdapter(connectionFactory, Queue("test_queue")))
+            .channel(inboundChannel)
+            .get()
+
+    @Bean
+    fun webSocketHandlerMapping(): HandlerMapping {
+        val map: HashMap<String, WebSocketHandler> = hashMapOf("/event-emitter" to webSocketHandler())
+        val handlerMapping = SimpleUrlHandlerMapping()
+        handlerMapping.order = 1
+        handlerMapping.urlMap = map
+        return handlerMapping
+    }
+
+    fun webSocketHandler() : WebSocketHandler {
+        val connections = HashMap<String, MessageHandler>()
+
+        return WebSocketHandler { session: WebSocketSession ->
+            val rabbit2webSocketFlux = Flux.create ( Consumer<FluxSink<WebSocketMessage>> {
+                val messageHandler = WebsocketMessageHandler(session, it)
+                connections[session.id] = messageHandler
+                inboundChannel().subscribe(messageHandler)
+            }).doFinally {
+                connections[session.id]?.apply {
+                    inboundChannel().unsubscribe(this)
+                    connections.remove(session.id)
+                }
+            }
+            session
+                    .send(rabbit2webSocketFlux)
+                    .and(session.receive())
+        }
+    }
+
+    @Bean
+    fun webSocketHandlerAdapter() = WebSocketHandlerAdapter()
+}
+
+
+fun main(args: Array<String>) {
+    SpringApplication.run(WebApp::class.java, *args)
+}
+
+class WebsocketMessageHandler(private val session: WebSocketSession, private val sink: FluxSink<WebSocketMessage>) : MessageHandler {
+    private val mapper = ObjectMapper()
+
+    init {
+        mapper.registerKotlinModule()
+    }
+
+    override fun handleMessage(msg: Message<*>) {
+        val event = mapper.readValue(msg.payload as ByteArray, Event::class.java)
+        sink.next(session.textMessage(mapper.writeValueAsString(event)))
+    }
+}
+
+data class Event(val test: String)
---
+++ src/test/java/de/codecentric/workshop/spring5/WebAppTest.kt
@@ -0,0 +1,74 @@
+package de.codecentric.workshop.spring5
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.hamcrest.Matchers.`is`
+import org.junit.Assert.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.web.reactive.socket.WebSocketSession
+import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient
+import org.springframework.web.reactive.socket.client.WebSocketClient
+import reactor.core.publisher.Flux
+import reactor.core.publisher.MonoProcessor
+import java.net.URI
+import java.time.Duration
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class WebAppTest {
+
+    @LocalServerPort
+    var port: Int? = null
+
+    final var mapper = ObjectMapper()
+
+    @Autowired
+    lateinit var client: WebSocketClient
+
+    @Autowired
+    lateinit var rabbbitTemplate: RabbitTemplate
+
+    init {
+        mapper.registerKotlinModule()
+    }
+
+    @Test
+    fun `sollte Event per Websocket empfangen`() {
+        val output : MonoProcessor<String> = MonoProcessor.create()
+
+        client.execute(URI.create("ws://localhost:$port/event-emitter"))
+            { session: WebSocketSession ->
+
+                rabbbitTemplate.convertAndSend("amq.topic", "test.queue",
+                        mapper.writeValueAsBytes(Event("max")))
+
+                session.send(Flux.empty())
+                        .and(session.receive()
+                                .map { it.payloadAsText }
+                                .subscribeWith(output)
+                                .then())
+            }.block(TIMEOUT)
+
+        assertThat(mapper.readValue(output.block(TIMEOUT), Event::class.java), `is`(Event("max")))
+    }
+
+}
+
+@Configuration
+class TestConfig {
+    @Bean
+    fun client(): ReactorNettyWebSocketClient {
+        return ReactorNettyWebSocketClient()
+    }
+}
+
+private val TIMEOUT= Duration.ofMillis(5000)
